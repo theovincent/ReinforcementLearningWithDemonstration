c6ada6a1b8164bfc659d0ab897a92aaffbcd8fa3
Théo <theo.vincent@eleves.enpc.fr>
2022-01-05 09:06:44 +0000

    Add end tabular DQfD

--- algorithms/DQfD/replay_buffer.py
+++ algorithms/DQfD/replay_buffer.py
@@ -46,18 +46,24 @@ class ReplayBuffer:
         self.last_collected_transition = None
         self.last_sampled_transition = None
 
+        self.intial_state_collected = False
+
     def collect_expert_trajectories(self, n_trajectories, policy):
         for idx_sample in range(n_trajectories):
-            self.env.reset()
+            self.env.state = choices(self.env._states)[0]
+            if self.env.state == self.env.initial_state_distribution:
+                self.intial_state_collected = True
+
             first_transition = True
             terminal = False
 
             while not terminal:
                 action = policy[self.env.state]
+                state = self.env.state
 
                 next_state, reward, terminal, _ = self.env.step(action)
 
-                self.buffer_expert.append(Transition(self.env.state, action, reward, next_state))
+                self.buffer_expert.append(Transition(state, action, reward, next_state))
                 if not first_transition:
                     self.buffer_expert[-2].next_transition = self.buffer_expert[-1]
 
@@ -65,18 +71,23 @@ class ReplayBuffer:
 
     def collect_rl_transition(self, Q):
         if self.last_collected_transition is None:
-            self.env.reset()
+            self.env.state = choices(self.env._states)[0]
+            if not self.intial_state_collected and self.env.state != self.env.initial_state_distribution:
+                self.env.state = self.env.initial_state_distribution
+                self.intial_state_collected = True
+
+        state = self.env.state
 
         if np.random.random() < self.epsilon_decay(len(self.buffer_rl) + len(self.buffer_expert)):
             action = np.random.choice(self.env._actions)
         else:
-            action = np.argmax([Q[self.env.state, action] for action in self.env._actions])
+            action = np.argmax([Q[state, action] for action in self.env._actions])
 
         next_state, reward, terminal, _ = self.env.step(action)
 
-        self.buffer_rl.append(Transition(self.env.state, action, reward, next_state))
+        self.buffer_rl.append(Transition(state, action, reward, next_state))
         if self.last_collected_transition is not None:
-            self.last_collected_transition.next_transition = self.buffer_expert[-1]
+            self.last_collected_transition.next_transition = self.buffer_rl[-1]
 
         if not terminal:
             self.last_collected_transition = self.buffer_rl[-1]
@@ -87,36 +98,51 @@ class ReplayBuffer:
         self.last_sampled_transition.td_loss = td_loss
 
     def sample_a_transition(self):
-        expert_probabilities = []
+        td_losses = []
+        number_occurences = np.zeros((self.env.S, self.env.A))
+
         for transition in self.buffer_expert:
-            expert_probabilities.append(
-                (np.abs(transition.td_loss) + self.prioritized_replay_expert_exploration)
-                ** self.prioritized_replay_exponent
-            )
+            number_occurences[transition.state, transition.action] += 1
+            if transition.td_loss is None:
+                td_losses.append(None)
+            else:
+                td_losses.append(
+                    (np.abs(transition.td_loss) + self.prioritized_replay_expert_exploration)
+                    ** self.prioritized_replay_exponent
+                )
 
-        rl_probabilities = []
         for transition in self.buffer_rl:
-            rl_probabilities.append(
-                (np.abs(transition.td_loss) + self.prioritized_replay_rl_exploration)
-                ** self.prioritized_replay_exponent
-            )
+            number_occurences[transition.state, transition.action] += 1
+            if transition.td_loss is None:
+                td_losses.append(None)
+            else:
+                td_losses.append(
+                    (np.abs(transition.td_loss) + self.prioritized_replay_rl_exploration)
+                    ** self.prioritized_replay_exponent
+                )
 
-        combined_probabilities = expert_probabilities + rl_probabilities
-        combined_probabilities /= np.sum(combined_probabilities)
+        td_losses_array = np.array(td_losses)
+        max_td_losses = np.amax(td_losses_array, where=td_losses_array != None, initial=1)
+        probabilities = np.where(td_losses_array == None, max_td_losses, td_losses_array)
 
-        idx_transition = choices(range(len(expert_probabilities + rl_probabilities)), combined_probabilities)
+        for idx_transition, transition in enumerate(self.buffer_expert + self.buffer_rl):
+            probabilities[idx_transition] /= number_occurences[transition.state, transition.action]
+
+        probabilities /= np.sum(probabilities)
+
+        idx_transition = choices(range(len(probabilities)), probabilities)[0]
 
         if idx_transition < len(self.buffer_expert):
             return (
                 self.buffer_expert[idx_transition],
                 True,
-                1 / (len(combined_probabilities) * combined_probabilities[idx_transition]) ** self.importance_sampling,
+                1 / (len(probabilities) * probabilities[idx_transition]) ** self.importance_sampling,
             )
         else:
             return (
                 self.buffer_rl[idx_transition - len(self.buffer_expert)],
                 False,
-                1 / (len(combined_probabilities) * combined_probabilities[idx_transition]) ** self.importance_sampling,
+                1 / (len(probabilities) * probabilities[idx_transition]) ** self.importance_sampling,
             )
 
     def sample_n_transitions(self):
@@ -124,7 +150,7 @@ class ReplayBuffer:
 
         self.last_sampled_transition = first_transition
         transitions = [first_transition]
-        n_transition = 0
+        n_transition = 1
 
         while n_transition < self.n_sted_td and not transitions[-1].next_transition is None:
             transitions.append(transitions[-1].next_transition)
@@ -132,17 +158,27 @@ class ReplayBuffer:
 
         return transitions, is_expert, weight
 
-    def display_statistics_on_transitions(self):
+    def display_statistics(self, transitions):
         number_occurences = np.zeros(self.env.S)
 
-        for transition in self.buffer_expert:
-            number_occurences[transition.state] += 1
-
-        for transition in self.buffer_rl:
+        for transition in transitions:
             number_occurences[transition.state] += 1
 
         img = self.env.get_layout_img(number_occurences)
         plt.figure()
         plt.title("Statistics on occurences")
+        plt.imshow(img)
+        plt.show()
+
+    def display_rl_statistics(self):
+        number_occurences = np.zeros(self.env.S)
+
+        for transition in self.buffer_rl:
+            number_occurences[transition.state] += 1
+
+        print("Max occurences", np.max(number_occurences))
+        img = self.env.get_layout_img(number_occurences)
+        plt.figure()
+        plt.title("Statistics on rl occurences")
         plt.imshow(img)
         plt.show()
--- algorithms/DQfD/tabular.py
+++ algorithms/DQfD/tabular.py
@@ -1,19 +1,18 @@
 import numpy as np
 
 
-def get_td_loss(transitions, env, Q, Q_target, gamma):
+def get_td_loss(transitions, env, Q, Q_target):
     return (
         transitions[0].reward
-        + gamma * np.max([Q_target[transitions[0].next_state, action] for action in env._actions])
+        + env.gamma * np.max([Q_target[transitions[0].next_state, action] for action in env._actions])
         - Q[transitions[0].state, transitions[0].action]
     )
 
 
 def get_n_step_td_loss(transitions, env, Q, Q_target, gammas):
     return (
-        np.sum([gammas[step] * transitions[step].reward for step in range(len(transitions) - 1)])
-        + gammas[len(transitions)]
-        * np.max([Q_target[transitions[len(transitions)].next_state, action] for action in env._actions])
+        np.sum([gammas[step] * transitions[step].reward for step in range(len(transitions))])
+        + gammas[len(transitions)] * np.max([Q_target[transitions[-1].next_state, action] for action in env._actions])
         - Q[transitions[0].state, transitions[0].action]
     )
 
@@ -43,20 +42,28 @@ def tabular_DQfD(
     n_td_loss_weight=1,
     expert_weight=1,
     diff_action_from_expert_penalisation=0.8,
+    show_policy=False,
+    show_value_function=False,
+    show_statistics=False,
+    display_frequency=1,
 ):
     from algorithms.DQfD.replay_buffer import ReplayBuffer
     from algorithms.epsilon_greedy import EpsilonDecay
 
-    from simulators.grid_world import GAMMA
-
     gammas = [1]
-    for pow_gamma in range(1, n_step_td):
-        gammas.append(GAMMA * gammas[-1])
+    for pow_gamma in range(1, n_step_td + 1):
+        gammas.append(env.gamma * gammas[-1])
+
+    # Average weights
+    sum_weights = td_loss_weight + n_td_loss_weight + expert_weight
+    td_loss_weight /= sum_weights
+    n_td_loss_weight /= sum_weights
+    expert_weight /= sum_weights
 
     if n_expert_trajectories > 0:
         from algorithms.VI_dynamic_programming import value_iteration
 
-        _, expert_policy = value_iteration(env.P, env.R, GAMMA)
+        _, expert_policy = value_iteration(env.P, env.R, env.gamma)
     else:
         expert_policy = None
 
@@ -68,13 +75,16 @@ def tabular_DQfD(
         n_step_td=n_step_td,
     )
 
-    Q = np.zeros((env._states, env._actions))
-    Q_target = np.zeros((env._states, env._actions))
+    Q = np.ones((env.Ns, env.Na)) * -150
+    Q_target = np.ones((env.Ns, env.Na)) * -150
 
-    for expert_iteration in range(n_expert_iterations):
+    for expert_iteration in range(1, n_expert_iterations + 1):
         transitions, _, weight = replay_buffer.sample_n_transitions()
 
-        td_loss = get_td_loss(transitions, env, Q, Q_target, GAMMA)
+        if show_statistics and expert_iteration % display_frequency == 0:
+            replay_buffer.display_statistics(transitions)
+
+        td_loss = get_td_loss(transitions, env, Q, Q_target)
         n_step_td_loss = get_n_step_td_loss(transitions, env, Q, Q_target, gammas)
         expert_loss = get_expert_loss(transitions, env, Q, Q_target, diff_action_from_expert_penalisation)
 
@@ -87,12 +97,28 @@ def tabular_DQfD(
         if expert_iteration % update_target_frequency == 0:
             Q_target = Q.copy()
 
-    for rl_iteration in range(n_rl_iterations):
+        if show_value_function and expert_iteration % display_frequency == 0:
+            env.display_value_function(Q)
+        if show_policy and expert_iteration % display_frequency == 0:
+            env.display_policy(Q)
+
+    print("End of expert phase")
+    if show_value_function:
+        env.display_value_function(Q)
+    if show_policy:
+        env.display_policy(Q)
+    print("Beginning of rl phase")
+
+    for rl_iteration in range(1, n_rl_iterations + 1):
         replay_buffer.collect_rl_transition(Q)
 
         transitions, is_expert, weight = replay_buffer.sample_n_transitions()
 
-        td_loss = get_td_loss(transitions, env, Q, Q_target, GAMMA)
+        if show_statistics and rl_iteration % display_frequency == 0:
+            replay_buffer.display_statistics(transitions)
+            replay_buffer.display_rl_statistics()
+
+        td_loss = get_td_loss(transitions, env, Q, Q_target)
         n_step_td_loss = get_n_step_td_loss(transitions, env, Q, Q_target, gammas)
         if is_expert:
             expert_loss = get_expert_loss(transitions, env, Q, Q_target, diff_action_from_expert_penalisation)
@@ -108,4 +134,9 @@ def tabular_DQfD(
         if rl_iteration % update_target_frequency == 0:
             Q_target = Q.copy()
 
-    return Q
+        if show_value_function and rl_iteration % display_frequency == 0:
+            env.display_value_function(Q)
+        if show_policy and rl_iteration % display_frequency == 0:
+            env.display_policy(Q)
+
+    return Q, replay_buffer
