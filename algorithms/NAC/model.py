c14142f4ef696e77cb2cfd0900b52917558fd466
Théo <theo.vincent@eleves.enpc.fr>
2022-01-27 21:37:34 +0000

    Cleaning ipynb

--- algorithms/NAC/model.py
+++ algorithms/NAC/model.py
@@ -1,33 +1,41 @@
 import numpy as np
+import torch
 import torch.nn as nn
+from torch.distributions.categorical import Categorical
 
 
-class SmallNAC:
+class SmallNAC(nn.Module):
     def __init__(self, env, entropy_weight):
+        super(SmallNAC, self).__init__()
         self.env = env
         self.entropy_weight = entropy_weight
 
-        self.net = nn.Sequential(
-            nn.Linear(3, 50), nn.ReLU(), nn.Linear(50, 1)  # 2 for the state and 1 for the action  # 1 for Q(s, a)
-        )
+        self.net = nn.Sequential(nn.Linear(124, 1), nn.Sigmoid())
+
+        self.net[0].weight.data = 0 * torch.ones(self.net[0].weight.data.size())
+        self.net[0].bias.data = -3.4 * torch.ones(self.net[0].bias.data.size())
 
     def Q(self, state, action):
-        (y, x) = self.env.index2coord[state]
-
-        return self.net(x, y, action)
+        return self.net(torch.tensor(self.env.get_feature(state, action), dtype=torch.float32))
 
     def V(self, state):
         v_value = 0
 
         for action in self.env._actions:
-            v_value += np.exp(self.Q(state, action) / self.entropy_weight)
+            v_value += torch.exp(self.Q(state, action) / self.entropy_weight)
 
-        return self.entropy_weight * np.log(v_value)
+        return self.entropy_weight * torch.log(v_value)
 
-    def pi(self, state):
-        pi_actions = np.zeros((self.env.Na))
+    def policy(self, state):
+        return self.pi_distribution(state).sample()
+
+    def pi_distribution(self, state):
+        pi_actions = torch.zeros((self.env.Na))
 
         for action in self.env._actions:
-            pi_actions[action] = np.exp((self.Q(state, action) - self.V(state)) / self.entropy_weight)
+            pi_actions[action] = torch.exp((self.Q(state, action) - self.V(state)) / self.entropy_weight)
 
-        return np.random.choice(self.env._actions, p=pi_actions)
+        return Categorical(probs=pi_actions)
+
+    def copy_to(self, target_model):
+        target_model.load_state_dict(self.state_dict())
--- algorithms/NAC/replay_buffer.py
+++ algorithms/NAC/replay_buffer.py
@@ -1,3 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 class ReplayBuffer:
-    def __init__(self):
-        pass
+    def __init__(self, env, n_expert_samples, expert_policy, batch_size):
+        self.env = env
+
+        self.buffer_expert = []
+        self.buffer_rl = []
+
+        if n_expert_samples > 0:  # i.e with Demonstration
+            self.collect_expert_samples(n_expert_samples, expert_policy)
+
+        self.batch_size = batch_size
+        self.rl_state = self.env.initial_state_distribution
+        self.terminal = False
+
+    def collect_expert_samples(self, n_samples, policy):
+        for idx_sample in range(n_samples):
+            state = np.random.choice(self.env._states) if idx_sample != 0 else self.env.initial_state_distribution
+            self.env.state = state
+
+            action = policy[state]
+
+            next_state, reward, _, _ = self.env.step(action)
+
+            self.buffer_expert.append((state, action, reward, next_state))
+
+    def collect_rl_sample(self, model):
+        if self.terminal:
+            self.rl_state = np.random.choice(self.env._states)
+        self.env.state = self.rl_state
+
+        action = model.policy(self.env.state).detach().numpy()
+
+        next_state, reward, self.terminal, _ = self.env.step(action)
+
+        self.buffer_rl.append((self.rl_state, action, reward, next_state))
+        self.rl_state = next_state
+
+    def get_batch(self, from_expert=True):
+        if from_expert:
+            return [
+                self.buffer_expert[np.random.choice(range(len(self.buffer_expert)))] for _ in range(self.batch_size)
+            ]
+        else:
+            return [self.buffer_rl[np.random.choice(range(len(self.buffer_rl)))] for _ in range(self.batch_size)]
+
+    def display_statistics(self, from_expert):
+        number_occurences = np.zeros(self.env.S)
+
+        if from_expert:
+            for (state, _, _, _) in self.buffer_expert:
+                number_occurences[state] += 1
+        else:
+            for (state, _, _, _) in self.buffer_rl:
+                number_occurences[state] += 1
+
+        img = self.env.get_layout_img(number_occurences)
+        plt.figure()
+        plt.title("Statistics on occurences")
+        plt.imshow(img)
+        plt.show()
